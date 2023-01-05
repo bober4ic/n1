9da42923958bbfaa92adf4ef238a1ee24a394fb4
bober4ic <nickoladent@gmail.com>
2023-01-05 09:05:21 +0200

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 46.29;
+        double b = 30.43;
+
+
+        System.out.println("с.ш.="+(a));
+        System.out.println("в.д.="+(b));
+    }
+}
